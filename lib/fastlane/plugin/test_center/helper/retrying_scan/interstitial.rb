8b24b6ce302e5fe7bc8c931f7a3ef83a5e382c0c
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2018-09-10 11:47:47 -0400

    reset sims code

--- lib/fastlane/plugin/test_center/helper/retrying_scan/interstitial.rb
+++ lib/fastlane/plugin/test_center/helper/retrying_scan/interstitial.rb
@@ -3,7 +3,7 @@ module TestCenter
     module RetryingScan
       class Interstitial
         def initialize(options)
-          @create_test_result_bundle = options[:test_result]
+          @create_test_result_bundle = options[:result_bundle]
           @output_directory = options[:output_directory]
           before_all
         end
@@ -18,6 +18,22 @@ module TestCenter
           glob_pattern = "#{@output_directory}/.*\.test_result"
           preexisting_test_result_bundles = Dir.glob(glob_pattern)
           FileUtils.rm_rf(preexisting_test_result_bundles)
+        end
+
+        def reset_simulators(destinations)
+          simulators = FastlaneCore::DeviceManager.simulators('iOS')
+          simulator_ids_to_reset = []
+          destinations.each do |destination|
+            destination.split(',').each do |destination_pair|
+              key, value = destination_pair.split('=')
+              if key == 'id'
+                simulator_ids_to_reset << value
+              end
+            end
+          end
+          simulators.each do |simulator|
+            simulator.reset if simulator_ids_to_reset.include?(simulator.udid)
+          end
         end
       end
     end
--- spec/retrying_scan/interstital_spec.rb
+++ spec/retrying_scan/interstital_spec.rb
@@ -1,20 +1,46 @@
 describe TestCenter::Helper::RetryingScan do
   describe 'interstitial', interstitial: true do
+    Interstitial = TestCenter::Helper::RetryingScan::Interstitial
+    
     it 'clears out `test_result` bundles when created' do
-      expected_calls = []
       allow(Dir).to receive(:glob).and_call_original
-      expect(Dir).to receive(:glob).with(/.*\.test_result/) do
-        expected_calls << :glob
-        ['./AtomicDragon.test_result']
-      end
       allow(FileUtils).to receive(:rm_rf).and_call_original
-      expect(FileUtils).to receive(:rm_rf).with(['./AtomicDragon.test_result']) do
-        expected_calls << :rm_rf
-      end
-      expect(expected_calls).to eq([:glob, :rm_rf, :correcting_scan, :correcting_scan])
+
+      expect(Dir).to receive(:glob).with(/.*\.test_result/).and_return(['./AtomicDragon.test_result'])
+      expect(FileUtils).to receive(:rm_rf).with(['./AtomicDragon.test_result'])
       Interstitial.new(
         result_bundle: true,
         output_directory: '.',
+      )
+    end
+
+    it 'resets a simulator between each run' do
+      stitcher = Interstitial.new(
+        output_directory: '.',
+      )
+      mock_devices = [
+        FastlaneCore::DeviceManager::Device.new(
+          name: 'iPhone Amazing',
+          udid: 'E697990C-3A83-4C01-83D1-C367011B31EE',
+          os_type: 'iOS',
+          os_version: '99.0',
+          state: 'Shutdown',
+          is_simulator: true
+        ),
+        FastlaneCore::DeviceManager::Device.new(
+          name: 'iPhone Bland',
+          udid: 'THIS-IS-A-UNIQUE-DEVICE-ID',
+          os_type: 'iOS',
+          os_version: '3.0',
+          state: 'Booted',
+          is_simulator: true
+        )
+      ]
+      allow(FastlaneCore::DeviceManager).to receive(:simulators).and_return(mock_devices)
+      expect(mock_devices[0]).to receive(:`).with('xcrun simctl erase E697990C-3A83-4C01-83D1-C367011B31EE')
+      expect(mock_devices[1]).not_to receive(:reset)
+      stitcher.reset_simulators(
+        ['platform=iOS Simulator,id=E697990C-3A83-4C01-83D1-C367011B31EE']
       )
     end
   end
