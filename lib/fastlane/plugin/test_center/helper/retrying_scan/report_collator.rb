d7cc6b4f629f0a28a97dc8a42703355b07a5db9c
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2018-09-28 10:11:35 -0400

    report collator html

--- lib/fastlane/plugin/test_center/helper/retrying_scan/report_collator.rb
+++ lib/fastlane/plugin/test_center/helper/retrying_scan/report_collator.rb
@@ -4,6 +4,8 @@ module TestCenter
       class ReportCollator
 
         CollateJunitReportsAction = Fastlane::Actions::CollateJunitReportsAction
+        CollateHtmlReportsAction = Fastlane::Actions::CollateHtmlReportsAction
+        
         def initialize(params)
           @output_directory = params[:output_directory]
           @reportnamer = params[:reportnamer]
@@ -14,6 +16,11 @@ module TestCenter
             File.absolute_path(relative_filepath)
           end
           file.sort! { |f1, f2| File.mtime(f1) <=> File.mtime(f2) }
+        end
+
+        def delete_globbed_intermediatefiles(glob)
+          retried_reportfiles = Dir.glob(glob)
+          FileUtils.rm_f(retried_reportfiles)
         end
 
         def create_config(klass, options)
@@ -31,6 +38,22 @@ module TestCenter
               }
             )
             CollateJunitReportsAction.run(config)
+            delete_globbed_intermediatefiles("#{@output_directory}/#{@reportnamer.junit_numbered_fileglob}")
+          end
+        end
+
+        def collate_html_reports
+          report_files = sort_globbed_files("#{@output_directory}/#{@reportnamer.html_fileglob}")
+          if report_files.size > 1
+            config = create_config(
+              CollateJunitReportsAction,
+              {
+                reports: report_files,
+                collated_report: File.absolute_path(File.join(@output_directory, @reportnamer.html_reportname))
+              }
+            )
+            CollateHtmlReportsAction.run(config)
+            delete_globbed_intermediatefiles("#{@output_directory}/#{@reportnamer.html_numbered_fileglob}")
           end
         end
       end
--- spec/retrying_scan/report_collator_spec.rb
+++ spec/retrying_scan/report_collator_spec.rb
@@ -7,7 +7,11 @@ describe TestCenter::Helper::RetryingScan do
     before(:each) do
       @mock_reportnamer = OpenStruct.new
       allow(@mock_reportnamer).to receive(:junit_fileglob).and_return('report*.xml')
+      allow(@mock_reportnamer).to receive(:html_fileglob).and_return('report*.html')
+      allow(@mock_reportnamer).to receive(:junit_numbered_fileglob).and_return("report-[1-9]*.xml")
+      allow(@mock_reportnamer).to receive(:html_numbered_fileglob).and_return("report-[1-9]*.html")
       allow(@mock_reportnamer).to receive(:junit_reportname).and_return('report.xml')
+      allow(@mock_reportnamer).to receive(:html_reportname).and_return('report.html')
     end
 
     it 'collates junit reports correctly' do
@@ -32,7 +36,35 @@ describe TestCenter::Helper::RetryingScan do
           }
         )
       end
+      expect(collator).to receive(:delete_globbed_intermediatefiles).with('./report-[1-9]*.xml')
       collator.collate_junit_reports
+    end
+
+    it 'collates html reports correctly' do
+      collator = ReportCollator.new(
+        output_directory: '.',
+        reportnamer: @mock_reportnamer
+      )
+      expect(collator).to receive(:sort_globbed_files).with('./report*.html').and_return(['report.html', 'report-1.html', 'report-2.html'])
+      config = OpenStruct.new
+      allow(config).to receive(:_values).and_return(
+        {
+            reports: ['report.html', 'report-1.html', 'report-2.html'],
+            collated_report: 'report.html'
+          }
+      )
+      expect(collator).to receive(:create_config).and_return(config)
+      expect(Fastlane::Actions::CollateHtmlReportsAction).to receive(:run) do |c|
+        expect(c._values).to eq(
+          {
+            reports: ['report.html', 'report-1.html', 'report-2.html'],
+            collated_report: 'report.html'
+          }
+        )
+      end
+      expect(collator).to receive(:delete_globbed_intermediatefiles).with('./report-[1-9]*.html')
+
+      collator.collate_html_reports
     end
   end
 end
