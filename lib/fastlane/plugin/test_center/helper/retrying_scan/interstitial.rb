3bf5a1688a173aec7eef6e8f9b53cb70da9071db
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2018-10-05 21:36:46 -0400

    WIP add methods to clone and rename sims

--- lib/fastlane/plugin/test_center/helper/retrying_scan/interstitial.rb
+++ lib/fastlane/plugin/test_center/helper/retrying_scan/interstitial.rb
@@ -123,3 +123,26 @@ module TestCenter
     end
   end
 end
+
+module FastlaneCore
+  class DeviceManager
+    class Device
+      def clone
+        raise 'Can only clone iOS Simulators' unless self.is_simulator
+
+        Device.new(
+          name: self.name,
+          udid: `xcrun simctl clone #{self.udid} '#{self.name}'`.chomp,
+          os_type: self.os_type,
+          os_version: self.os_version,
+          state: self.state,
+          is_simulator: self.is_simulator
+        )
+      end
+
+      def rename(newname)
+        `xcrun simctl rename #{self.udid} '#{newname}''`
+      end
+    end
+  end
+end
